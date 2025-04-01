883b1137be3b46c64014b933f0fd566b479c5542
logic-uni <yuhaozhang24@gmail.com>
2025-04-01 15:38:33 +0800

    new file:   Modeling/Generate_sudo_spike_train.py 	deleted:    Modeling/laila.py 	renamed:    Modeling/heihe.py -> Modeling/waiting_bus_syt2_version2.py

---
+++ Modeling/Generate_sudo_spike_train.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import expon, gamma
+from elephant.spike_train_generation import homogeneous_poisson_process
+from viziphant.statistics import plot_isi_histogram
+
+

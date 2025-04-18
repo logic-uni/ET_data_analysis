46a64a09a9e93fdbaa25ed5b3387dff87936b945
logic-uni <yuhaozhang24@gmail.com>
2025-04-18 22:53:08 +0800

    modified:   Oscillation/Fr_osci.py

--- Oscillation/Fr_osci.py
+++ Oscillation/Fr_osci.py
@@ -11,47 +11,60 @@ import numpy as np
 import pandas as pd
 from matplotlib.pyplot import *
 from ast import literal_eval
-from sklearn.decomposition import PCA
 from elephant.conversion import BinnedSpikeTrain
 np.set_printoptions(threshold=np.inf)
 np.seterr(divide='ignore',invalid='ignore')
 
+fr_bin = 10  # unit: ms
+fs = 
+freq_low, freq_high = 4, 16
+
+### ------------------ Load Data-------------------
+
+## --------- NP2 ----------
+mice_name = '20250310_VN_tremor'
+sorting_path = f"/data1/zhangyuhao/xinchao_data/NP2/{mice_name}/Sorted/"
+save_path = f"/home/zhangyuhao/Desktop/Result/ET/Fr_FFT/NP1/Easysort/{mice_name}/"
+sample_rate = 30000 #spikeGLX neuropixel sample rate
+identities = np.load(sorting_path + '/spike_clusters.npy') # time series: unit id of each spike
+times = np.load(sorting_path + '/spike_times.npy')  # time series: spike time of each spike
+neurons = pd.read_csv(sorting_path + '/cluster_group.tsv', sep='\t')  
+print(neurons)
+elec_dura = times[-1] / sample_rate
+marker = pd.read_csv(f"/data1/zhangyuhao/xinchao_data/NP2/{mice_name}/Marker/static_motion_segement.csv")
+print(marker)
+print("Test if electrophysiology duration is equal to treadmill duration ...")
+print(f"Electrophysiology duration: {elec_dura}")
+print(f"marker duration: {marker['time_interval_right_end'].iloc[-1]}")
+
+## --------- NP1 ----------
+mice_name = '20230602_Syt2_conditional_tremor_mice2_lateral'
+region_name = 'Lobule III'
+# Regeion List
 # Lobule
 #20230113_littermate    Lobules IV-V
 #20230523_Syt2_conditional_tremor_mice1    Lobule III  Lobule II
 #20230604_Syt2_conditional_tremor_mice2_medial   Lobule III
 #20230602_Syt2_conditional_tremor_mice2_lateral  Lobule III
 #20230623_Syt2_conditional_tremor_mice4  Lobule III  Lobule II
-#  DCN
+# DCN
 #20230604_Syt2_conditional_tremor_mice2_medial  xinchaosort  Vestibulocerebellar nucleus
 #20230113_littermate   Interposed nucleus
-
-region_name = 'Lobule III'
-fr_bin = 1  # unit: ms
-avoid_spikemore1 = True # 避免1ms的bin里有多个spike,对于1ms内多个spike的，强行置1
-
-## ------ Easysort ------
-# --- NEED CHANGE ---
-mice_name = '20230602_Syt2_conditional_tremor_mice2_lateral'
+# ------ Easysort ------  With QC
+#  NEED CHANGE
 mapping_file = 'unit_ch_dep_region_QC_isi_violations_ratio_pass_rate_60.17316017316017%.csv'
-QC_method = 'Without_QC'  # Without_QC/QC_ISI_violation/etc
-# --- NO NEED CHANGE ---
-sorting_path = rf'E:\xinchao\Data\useful_data\NP1\{mice_name}\Sorted\Easysort\results_KS2\sorter_output'
-save_path = rf'C:\Users\zyh20\Desktop\Research\01_ET_data_analysis\Research\spectrum_analysis\NP1\Easysort\{QC_method}\{mice_name}'  
-neurons = pd.read_csv(rf'E:\xinchao\Data\useful_data\NP1\{mice_name}\Sorted\Easysort\mapping\{mapping_file}')  # different sorting have different nueron id
-
-# ------ Xinchao_sort ------
-# --- NEED CHANGE ---
-#mice_name = '20230602_Syt2_conditional_tremor_mice2_lateral'
-# --- NO NEED CHANGE ---
-#sorting_path = rf'E:\xinchao\Data\useful_data\NP1\{mice_name}\Sorted\Xinchao_sort'
-#save_path = rf'C:\Users\zyh20\Desktop\Research\01_ET_data_analysis\Research\spectrum_analysis\NP1\Xinchao_sort\{mice_name}'  
+#  NO NEED CHANGE
+sorting_path = f"/data1/zhangyuhao/xinchao_data/NP1/{mice_name}/Sorted/Easysort/results_KS2/sorter_output/"
+save_path = f"/home/zhangyuhao/Desktop/Result/ET/Fr_FFT/NP1/Easysort/{mice_name}/"
+neurons = pd.read_csv(f"/data1/zhangyuhao/xinchao_data/NP1/{mice_name}/Sorted/Easysort/mapping/")  # different sorting have different nueron id
+# ------ Xinchao_sort ------  Without QC
+#  NO NEED CHANGE 
+#sorting_path = f"/data1/zhangyuhao/xinchao_data/NP1/{mice_name}/Sorted/Xinchao_sort/"
+#save_path = "/home/zhangyuhao/Desktop/Result/ET/Fr_FFT/NP1/Xinchao_sort/{mice_name}/"
 #neurons = pd.read_csv(sorting_path + '/neuron_id_region_firingrate.csv')  # different sorting have different nueron id
-
-# --------- Main Program ----------
-treadmill = pd.read_csv(rf'E:\xinchao\Data\useful_data\NP1\{mice_name}\Marker\treadmill_move_stop_velocity_segm_trial.csv',index_col=0)
-treadmill_origin = pd.read_csv(rf'E:\xinchao\Data\useful_data\NP1\{mice_name}\Marker\treadmill_move_stop_velocity.csv',index_col=0)
-# electrophysiology
+#  ------ Common ------ electrophysiology & behavior
+treadmill = pd.read_csv(f"/data1/zhangyuhao/xinchao_data/NP1/{mice_name}/Marker/treadmill_move_stop_velocity_segm_trial.csv",index_col=0)
+treadmill_origin = pd.read_csv(f"/data1/zhangyuhao/xinchao_data/NP1/{mice_name}/Marker/treadmill_move_stop_velocity.csv",index_col=0)
 sample_rate = 30000 #spikeGLX neuropixel sample rate
 identities = np.load(sorting_path + '/spike_clusters.npy') # time series: unit id of each spike
 times = np.load(sorting_path + '/spike_times.npy')  # time series: spike time of each spike
@@ -62,7 +75,8 @@ treadmill_dura = treadmill_origin['time_interval_right_end'].iloc[-1]
 print(f"Electrophysiology duration: {elec_dura}")
 print(f"Treadmill duration: {treadmill_dura}")
 
-# get single neuron spike train
+### ------------------ Main Program -------------------
+
 def singleneuron_spiketimes(id):
     x = np.where(identities == id)
     y=x[0]
@@ -72,79 +86,48 @@ def singleneuron_spiketimes(id):
         spike_times[i]=times[z]/sample_rate
     return spike_times
 
-def popu_sptrain_trial(neuron_ids,marker_start,marker_end):
+def fr_trial(neuron_ids,marker_start,marker_end):
     for j in range(len(neuron_ids)): #第j个neuron
         spike_times = singleneuron_spiketimes(neuron_ids[j])
         spike_times_trail = spike_times[(spike_times > marker_start) & (spike_times < marker_end)]
         spiketrain = neo.SpikeTrain(spike_times_trail,units='sec',t_start=marker_start, t_stop=marker_end)
         fr = BinnedSpikeTrain(spiketrain, bin_size=fr_bin*pq.ms,tolerance=None)
-        if avoid_spikemore1 == False:
-            one_neruon = fr.to_array().astype(int)[0]
-        else:
-            fr_binar = fr.binarize()  # 对于可能在1ms内出现两个spike的情况，强制置为该bin下即1ms只能有一个spike
-            one_neruon = fr_binar.to_array().astype(int)[0]
+        trial_neruon_fr = fr.to_array().astype(int)[0]
         
-        if j == 0:
-            neurons = one_neruon
-        else:
-            neurons = np.vstack((neurons, one_neruon))
-    return neurons
+        # Perform FFT on trial_neruon_fr
+        fft_result = np.fft.fft(trial_neruon_fr)
+        freqs = np.fft.fftfreq(len(trial_neruon_fr), d=fr_bin / 1000.0)  # Convert bin size to seconds
 
-def spectrum_analysis(region,popu_ids):
-    neuron_num = len(popu_ids)
-    squmat_interval = neuron_num  #单位ms
-    trial_spiketr_len = 30000/fr_bin
-    truncated_trial = int(trial_spiketr_len - (trial_spiketr_len % squmat_interval))   # 一个trial切分成多个小正方形矩阵，余下的不要
-    for index, row in treadmill.iterrows():
-        trial_start = row['time_interval_left_end']
-        trial_end = row['time_interval_right_end']
-        status = row['run_or_stop']
-        if status == 0:
-            color = 'b'
-        else:
-            color = 'r'
-        data = popu_sptrain_trial(popu_ids,trial_start,trial_end)
-        print(data.shape)
-        for squleft in range(0,squmat_interval,truncated_trial):
-            square_data = data[:,squleft:squleft+squmat_interval]
-            print(square_data.shape)
-            eigenvalues, eigenvectors = np.linalg.eig(square_data)
-            print(np.abs(eigenvalues))
-            plt.scatter(eigenvalues.real, eigenvalues.imag, c=color, marker='o')
-    
-    plt.title(f'spectrum_analysis_{region}')
-    plt.text(0.05, 0.95, f'Each Truncated Time Length: {squmat_interval} ms', 
-                    ha='left', va='top', transform=plt.gca() .transAxes, fontsize=10)
-    plt.savefig(save_path+f"/{region}.png",dpi=600,bbox_inches = 'tight')
-    plt.clf()
+        # Focus on the frequency range of interest
+        freq_mask = (freqs >= freq_low) & (freqs <= freq_high)
+        fft_filtered = fft_result[freq_mask]
+        freqs_filtered = freqs[freq_mask]
+
+        # Plot the FFT result
+        plt.figure(figsize=(10, 6))
+        plt.plot(freqs_filtered, np.abs(fft_filtered), label=f'Neuron {neuron_ids[j]}')
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Amplitude')
+        plt.title(f'FFT of Neuron {neuron_ids[j]} (Trial {marker_start}-{marker_end})')
+        plt.legend()
+        plt.grid()
+        plt.show()
 
 def main():
-    # 谱分析画布固定内容
-    plt.figure(figsize=(10, 10))
-    plt.xlabel('Real')
-    plt.xlim(-5,5)
-    plt.ylim(-5,5)
-    plt.ylabel('Imaginary')
-    plt.grid(True)
-    plt.gca().set_aspect('equal')
-    theta = np.linspace(0, 2 * np.pi, 100)
-    x = 1 * np.cos(theta)
-    y = 1 * np.sin(theta)
-    plt.plot(x, y,c='black')
-
     result = neurons.groupby('region')['cluster_id'].apply(list).reset_index(name='cluster_ids')
     print(result)
     for index, row in result.iterrows():
         region = row['region']
         popu_ids = row['cluster_ids']
         if region == region_name:
-            spectrum_analysis(region,popu_ids)
-
-    #max_eigen1 = covmatr_spectrum(data[:,(pert_s-40):(pert_s)])
-    #max_eigen2 = covmatr_spectrum(data[:,(pert_s):(pert_s+40)])
-    #eigen_vector_included_angle(max_eigen1,max_eigen2)
-    #covmatr_spectrum(data[:,(pert_s-40):(pert_s)])
-    #covmatr_spectrum(data[:,(pert_s-40):(pert_e+150)])
-    #SVD_matrix_spectrum(data[:,(pert_s):(pert_s+40)])
+            for index, row in treadmill.iterrows():
+                trial_start = row['time_interval_left_end']
+                trial_end = row['time_interval_right_end']
+                status = row['run_or_stop']
+                if status == 0:
+                    color = 'b'
+                else:
+                    color = 'r'
+                data = fr_trial(popu_ids,trial_start,trial_end)
 
 main()
